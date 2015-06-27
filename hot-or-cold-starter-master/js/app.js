d194222b893502d1336002196b25891e7875ed10
n8kolenberg <nkolenberg@gmail.com>
2015-06-27 17:04:19 +0100

    Add if statements to define hot or cold status of guess

--- hot-or-cold-starter-master/js/app.js
+++ hot-or-cold-starter-master/js/app.js
@@ -15,13 +15,14 @@ $(function(){
 
 	/*-- Variables --*/
   	var $userGuess;
-  	var $feedback = $('#feedback');
   	var randomNumber = 0;
   	var amountOfGuesses = 0;
   	var guessCorrect = false;
 
+
   	/*-- Initialise New Game --*/
   	newGame();
+
 
  	/*-- New Game Function to encompass all code--*/
 	function newGame () {
@@ -30,17 +31,17 @@ $(function(){
 		resetGuessCount();
 		clearPrevGuesses();
 		randomNumber = pickRandomNumber();
-		$feedback.html('Make your Guess!');
+		setFeedback('Make your Guess!');
 		guessCorrect = false;
 
 	} // End New Game
 	
 
+	/*--Restarting the game by clicking '+New Game'--*/
 	$('#newGame').on('click', function(event){
 		event.preventDefault();
 		newGame();
 	}); //End on Click New Game
-
 
 
    	/*-- Select a random numer --*/
@@ -50,24 +51,34 @@ $(function(){
   		return randomNumber;
   	};
 
+
   	/*--Function to set focus on input--*/
   	function setFocus () {
   		$('#userGuess').focus();
   	}
+
 
   	/*--Function to clear the userGuess input field--*/
   	function clearPrevInput () {
   		$('#userGuess').val('');
   	}
 
+
 	/*--Function to reset the amount of user guesses--*/
   	function resetGuessCount () {
   		$('#count').html('0');
   	}
 
+
 	/*--Function to clear the previous user guesses--*/
   	function clearPrevGuesses () {
   		$('ul#guessList li').remove();
+  	}
+
+
+  	/*-- Set Feedback function --*/
+  	function setFeedback (feedback) {
+  		$('#feedback').html(feedback);
   	}
 
   	/*-- Accepting user input and providing feedback --*/
@@ -79,12 +90,13 @@ $(function(){
   			if(!guessCorrect) {
 	  			$userGuess = Number($('#userGuess').val());
 	  			validateUserInput($userGuess);
+	  			console.log("User Guess: " + $userGuess);
 	  			//After user guesses, remove input val and put focus on input
 	  			clearPrevInput();
 	  			setFocus();
-	  			return compare();  	
+	  			return compare();	
 		  } else {
-		  	$feedback.html("You've already guessed correctly. Click '+New Game' to play again");
+		  	setFeedback("You've already guessed correctly. Click '+New Game' to play again");
 		  } //End if statement
 
 	}); //End Submit on form
@@ -95,7 +107,7 @@ $(function(){
   		/*If the user doesn't input a number or  
   		  inputs anything higher than 100 or below or equal to 0 */
   		if( isNaN(input) || input > 100 || input <= 0) {
-  			$feedback.html('Please enter a number between 1 and 100');
+  			setFeedback('Please enter a number between 1 and 100');
   		} else {
 	  		//If user successfully inputs number, we add to the amount of guesses and show it
 	  		amountOfGuesses++;
@@ -108,16 +120,36 @@ $(function(){
 
 	//To check whether userGuess is equal to randomNumber
 	function compare () {
-		if ($userGuess > randomNumber) {
-			$feedback.html("Your number is too high");
-		} else if ($userGuess < randomNumber) {
-			$feedback.html("Your number is too low");
-		} else {
-			$feedback.html('You got it');
-			guessCorrect = true;
-		}
+		var guessDifference = randomNumber - $userGuess;
+		guessDifference = Math.abs(guessDifference);
+		console.log('difference: ' + guessDifference);
+				
+		if (guessDifference >= 70) {
+			setFeedback('Your guess is getting frozen over');
+		
+		} else if (guessDifference >=60 && guessDifference <70) {
+			setFeedback('Your guess is getting colder');
+		
+		} else if (guessDifference >= 50 && guessDifference <60) {
+			setFeedback('Your guess is getting chilly');
 
-	};//End compare function
+		} else if (guessDifference >= 30 && guessDifference <50) {
+			setFeedback('Your guess is getting hot');
+
+		} else if (guessDifference >= 10 && guessDifference <30) {
+			setFeedback('Your guess is getting hotter');
+
+		} else if (guessDifference >= 1 && guessDifference <10) {
+			setFeedback('Your guess is getting too hot to handle');
+		
+		} else {
+			setFeedback('Yeah baby!! You got it!');
+			guessCorrect = true;
+		
+		}//End if statement
+
+	}//End compare function
+
 
 }); //End ready
 
