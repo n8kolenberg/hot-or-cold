7a8da1f0baf7cfd20ec8be382262780111ee97eb
n8kolenberg <nkolenberg@gmail.com>
2015-06-27 15:37:00 +0100

    Focus on refactoring actions, fix submit and add simple compare function to build upon

--- hot-or-cold-starter-master/js/app.js
+++ hot-or-cold-starter-master/js/app.js
@@ -13,46 +13,88 @@ $(function(){
   	});
 
 
-  	/*-- New Game Function --*/
-  	// function newGame () {
-  	// 	pickRandomNumber();
-  	// 	catchUserGuess();
-  	// }
-
-
-  	/*-- Variables --*/
+	/*-- Variables --*/
   	var $userGuess;
   	var $feedback = $('#feedback');
   	var randomNumber = 0;
   	var amountOfGuesses = 0;
+  	var guessCorrect = false;
+
+  	/*-- Initialise New Game --*/
+  	newGame();
+
+ 	/*-- New Game Function to encompass all code--*/
+	function newGame () {
+		setFocus();
+		clearPrevInput();
+		resetGuessCount();
+		clearPrevGuesses();
+		randomNumber = pickRandomNumber();
+		$feedback.html('Make your Guess!');
+		guessCorrect = false;
+
+	} // End New Game
+	
+
+	$('#newGame').on('click', function(event){
+		event.preventDefault();
+		newGame();
+	}); //End on Click New Game
 
 
-  	/*-- Select a random numer --*/
+
+   	/*-- Select a random numer --*/
   	function pickRandomNumber () {
   		randomNumber= Math.floor(Math.random()*100)+1;
+  		console.log("Random Number Generated: " + randomNumber);
   		return randomNumber;
+  	};
+
+  	/*--Function to set focus on input--*/
+  	function setFocus () {
+  		$('#userGuess').focus();
+  	}
+
+  	/*--Function to clear the userGuess input field--*/
+  	function clearPrevInput () {
+  		$('#userGuess').val('');
+  	}
+
+	/*--Function to reset the amount of user guesses--*/
+  	function resetGuessCount () {
+  		$('#count').html('0');
+  	}
+
+	/*--Function to clear the previous user guesses--*/
+  	function clearPrevGuesses () {
+  		$('ul#guessList li').remove();
   	}
 
   	/*-- Accepting user input and providing feedback --*/
-
-  	(function catchUserGuess () {
-	  	$('#guessForm').on('keydown', '#userGuess', function(event){
-	  		if(event.keyCode == 13) {
-	  			$userGuess = Number($('#userGuess').val());
-	  			validateForNaN($userGuess);
-	  			event.preventDefault();
-	  			
-	  			return console.log($userGuess);
-	  		} //End if statment
 	  	
-	  	}); //End on keydown
-	})(); //End catchUserGuess
+	  	//Method to get user's guess through clicking 'guess' button
+	  	$('#guessForm').submit(function(event){
+	  		event.preventDefault();
+
+  			if(!guessCorrect) {
+	  			$userGuess = Number($('#userGuess').val());
+	  			validateUserInput($userGuess);
+	  			//After user guesses, remove input val and put focus on input
+	  			clearPrevInput();
+	  			setFocus();
+	  			return compare();  	
+		  } else {
+		  	$feedback.html("You've already guessed correctly. Click '+New Game' to play again");
+		  } //End if statement
+
+	}); //End Submit on form
 
 
   	/*-- Validation functions --*/
-  	function validateForNaN (input) {
-  		// $('#feedback').html('');
-  		if( isNaN(input) ) {
+  	function validateUserInput (input) {
+  		/*If the user doesn't input a number or  
+  		  inputs anything higher than 100 or below or equal to 0 */
+  		if( isNaN(input) || input > 100 || input <= 0) {
   			$feedback.html('Please enter a number between 1 and 100');
   		} else {
 	  		//If user successfully inputs number, we add to the amount of guesses and show it
@@ -63,6 +105,20 @@ $(function(){
   		}
   	} //End validateForNaN
 
-});
+
+	//To check whether userGuess is equal to randomNumber
+	function compare () {
+		if ($userGuess > randomNumber) {
+			$feedback.html("Your number is too high");
+		} else if ($userGuess < randomNumber) {
+			$feedback.html("Your number is too low");
+		} else {
+			$feedback.html('You got it');
+			guessCorrect = true;
+		}
+
+	};//End compare function
+
+}); //End ready
 
 
